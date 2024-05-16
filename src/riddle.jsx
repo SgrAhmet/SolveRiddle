f7c9d84d041a7b70691d435f658b6f0910ab64ee
SgrAhmet <ahmet1aydos@gmail.com>
2024-05-16 21:23:42 +0300

    newmethod

--- src/riddle.jsx
+++ src/riddle.jsx
@@ -46,23 +46,55 @@ const Riddle = () => {
 
     // console.log(document.getElementById(`${e.target.innerHTML}`));
 
-    // controlAll()
+    controlAll()
   };
 
-  // const controlAll = () =>{
+  const controlAll = () =>{
 
-  //   for (let i = 2; i <= 23; i++) {
-  //     // console.log(document.getElementById(`${i}`).classList.value);
-  //     const fragments = [];
-  //     for (let i = 1; i < num; i++) {
-  //       if (num % i === 0) {
-  //         fragments.push(i);
-  //       }
-  //     }
-  //   }
+  
+    //  let ControlNumber = 11
+    for (let ControlNumber = 2; ControlNumber < 24; ControlNumber++) {
+      const fragments = [];
+    for (let i = 1; i < ControlNumber; i++) {
+      if (ControlNumber % i === 0) {
 
 
-  // }
+        if (!document.getElementById(`${i}`).classList.value) {
+        
+        fragments.push(i);
+          
+        }
+
+      }
+    }
+    
+    if (fragments.length == 0) {
+      console.log(ControlNumber + " fragments yok : " + fragments)
+
+      if (document.getElementById(`${ControlNumber}`).classList.value != "red") {
+
+        if (document.getElementById(`${ControlNumber}`).classList.value != "green") {
+
+          const turnRed =()=>{
+            document.getElementById(`${ControlNumber}`).classList.add("red")
+          }
+
+          setTimeout(turnRed, 3000)
+          
+          // console.log(document.getElementById(`${ControlNumber}`).classList)
+        }
+                
+      }
+    } else {
+      console.log(ControlNumber +" fragments var : " + fragments)
+      
+      
+    }
+    }
+        
+   
+
+  }
 
   const buttonsArray = [];
   const renderButtons = (numOfButtons) => {
@@ -95,6 +127,90 @@ const Riddle = () => {
 
   // console.log("type of num is :" + typeof(num))
   // console.log("type of string is :" + typeof(string))
+
+
+
+
+
+
+  // const myPow =(x,y)=> {
+  //   let sum = 1
+  
+  // if(y % 2 === 0){
+  // for (let i = 1; i <= y/2; i++) {
+  //     sum *= x*x
+  //   }
+  // }else{
+  //   y = y- 1
+  //   for (let i = 1; i <= y/2; i++) {
+  //     sum *= x*x
+      
+  //   }
+  //   sum = sum * x
+  // }  
+  // return sum
+  // }
+  
+  
+
+
+
+
+
+
+
+  
+  
+  
+//  const newMyPow =(x,y)=>{
+//   let sum = 1
+//   for (let i = 1; i <= y; i++) {
+//       sum = sum * x
+//   }
+//   return sum
+//  }
+
+//  console.log(newMyPow(7,0))
+
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+    
+//     const findFragmnets =(num)=>{
+        
+//         let fragmnets = []
+        
+//         for (let i = 2 ; i < num ; i++){
+//             if(num % i == 0){
+//                     fragmnets.push(i)
+                    
+//             }
+//         }
+        
+//         if (fragmnets.length == 0) {
+//         console.log(num + " ın bölenleri yok")
+          
+//         }else{
+//           console.log(num + " ın bölenleri bunlar "+fragmnets)
+
+//         }
+
+
+
+        
+//         for(let i = 0 ; i < fragmnets.length ; i++){
+//             findFragmnets(fragmnets[i]);
+//         }
+        
+        
+       
+//     }
+    
+//     findFragmnets(22);
+
+
+
+
+
 
   return (
     <div>
